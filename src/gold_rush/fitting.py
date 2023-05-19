1e79990165630c77aea45093b46c677be31629f5
Lisa McBride <lisaleemcb@gmail.com>
2023-05-19 14:10:37 -0700

    first commit of actual code

---
+++ src/gold_rush/fitting.py
@@ -0,0 +1,58 @@
+import time
+import numpy as np
+import emcee
+
+from multiprocessing import Pool
+
+def log_prior(params):
+    omegab = params[0]
+    omegac = params[1]
+    h_fid = params[2]
+    As = params[3]
+    ns = params[4]
+    tau_fid = params[5]
+
+    if 1.8e-09 < As < 2.5e-09:
+        return 0.0
+    # if -5.0 < omegab < 0.5 and 0.0 < b < 10.0 and -10.0 < log_f < 1.0:
+    #     return 0.0
+    return -np.inf
+
+def log_likelihood(params, data, model, sigmas):
+    log_likelihood = -(data - model(params))**2 / sigmas**2
+    return log_likelihood.sum()
+
+def log_probability(params, data, model, sigmas):
+    lp = log_prior(params)
+    if not np.isfinite(lp):
+        return -np.inf
+
+    #t0 = time.time()
+    lklhd = log_likelihood(params, data, model, sigmas)
+    #tf = time.time()
+
+    #print(f'likelihood evaluation took {(tf-t0):.3f} seconds with value {lp + lklhd}')
+
+    return lp + lklhd
+
+def start_mcmc(truths, data, model, sigmas, nwalkers=36, nsteps=1e5, burn_in=50):
+    t0 = time.time()
+    ndim = truths.size
+    print(f'running emcee for {nsteps} steps with {nwalkers} walkers now with multiprocessing!...')
+    backend = emcee.backends.HDFBackend('test.h5')
+    pos = truths * np.ones((nwalkers, ndim)) + 1e-10 * np.random.randn(nwalkers, ndim)
+
+    sampler = emcee.EnsembleSampler(
+        nwalkers, ndim, log_probability,
+        backend=backend, args=(data, model, sigmas)
+    )
+    state = sampler.run_mcmc(pos, burn_in)
+    sampler.reset()
+    sampler.run_mcmc(state, nsteps)
+
+    sampler.run_mcmc(pos, nsteps, progress=True)
+
+    tf = time.time()
+    print(f'mcmc sampling took {(tf-t0)/60/60:.3f} hours')
+
+    return sampler
---
+++ src/gold_rush/model.py
@@ -0,0 +1,104 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import zeus21
+
+#set up the CLASS cosmology
+from classy import Class
+
+class Model:
+    def __init__(self, params, z=13, verbose=False):
+        self.omegab = params[0]
+        self.omegac = params[1]
+        self.h_fid = params[2]
+        self.As = params[3]
+        self.ns = params[4]
+        self.tau_fid = params[5]
+
+        # create cosmoparams object to pass to classy
+        self.CosmoParams_input = zeus21.Cosmo_Parameters_Input(
+                                            omegab=self.omegab,
+                                            omegac=self.omegac,
+                                            h_fid=self.h_fid,
+                                            As=self.As,
+                                            ns=self.ns,
+                                            tau_fid=self.tau_fid)
+        self.z = z
+        self.verbose = verbose
+
+        # Now all the initialisations
+        self.ClassCosmo = Class()
+        self.ClassCosmo.compute()
+        self.ClassyCosmo = zeus21.runclass(self.CosmoParams_input)
+        if verbose:
+            t0 = time.time()
+            print('CLASS has run, we store the cosmology.')
+
+        self.CosmoParams = zeus21.Cosmo_Parameters(self.CosmoParams_input,
+                                                   self.ClassyCosmo)
+        self.CorrFClass = zeus21.Correlations(self.CosmoParams, self.ClassyCosmo)
+        if verbose:
+            print('Correlation functions saved.')
+        self.HMFintclass = zeus21.HMF_interpolator(self.CosmoParams, self.ClassyCosmo)
+        if verbose:
+            print('HMF interpolator built. This ends the cosmology part -- moving to astrophysics.')
+
+        #set up your astro parameters too, here the peak of f*(Mh) as an example
+        self.AstroParams = zeus21.Astro_Parameters(self.CosmoParams)
+
+        ZMIN = 10.0 #down to which z we compute the evolution
+        self.CoeffStructure = zeus21.get_T21_coefficients(self.CosmoParams, self.ClassyCosmo,
+                                        self.AstroParams, self.HMFintclass, zmin=ZMIN)
+        if verbose:
+            print('SFRD and coefficients stored. Move ahead.')
+
+        self.zlist = self.CoeffStructure.zintegral
+        RSDMODE = 1 #which RSD mode you want, 0 is no RSDs (real space), 1 is spherical (as simulations usually take), 2 is mu~1 (outside the wedge, most relevant for observations)
+        self.PS21 = zeus21.Power_Spectra(self.CosmoParams, self.ClassyCosmo, self.CorrFClass,
+         self.CoeffStructure, RSD_MODE = RSDMODE)
+        self.klist = self.PS21.klist_PS
+
+        if verbose:
+            print('Computed the 21-cm power spectrum. Initialisation complete!')
+            tf = time.time()
+            print(f'took {(tf-t0):.3f} seconds')
+
+    def gen_PS21(self):
+        _iz = min(range(len(self.zlist)), key=lambda i: np.abs(self.zlist[i]-self.z))
+
+        return self.PS21.Deltasq_T21[_iz]
+
+
+    def plot_PS21(self, choice, k_or_z='k'):
+        fig, ax = plt.subplots(figsize=(5,3))
+
+        if k_or_z == 'k':
+            #choose a k to plot
+            kchoose=choice # 0.3;
+            _ik = min(range(len(self.klist)), key=lambda i: np.abs(self.klist[i]-kchoose))
+
+            ax.semilogy(self.zlist, self.PS21.Deltasq_T21[:,_ik], color='k', linewidth=2.0)
+            ax.semilogy(self.zlist, self.PS21.Deltasq_T21_lin[:,_ik], color='gray', linewidth=1.2)
+
+            ax.set_xlabel(r'$z$');
+            ax.set_ylabel(r'$\Delta^2_{21}\,\rm[mK^2]$');
+            ax.legend([r'Full Zeus21', r'Linear'])
+
+            ax.set_xlim([10, 25])
+            ax.set_ylim([1,200])
+
+        if k_or_z == 'z':
+            #choose a z to plot
+            zchoose=choice # 13.;
+            _iz = min(range(len(self.zlist)), key=lambda i: np.abs(self.zlist[i]-zchoose))
+
+            ax.loglog(self.klist, self.PS21.Deltasq_T21[_iz], color='k', linewidth=2.0)
+            ax.loglog(self.klist, self.PS21.Deltasq_T21_lin[_iz], color='gray', linewidth=1.2)
+
+            ax.set_xlabel(r'$k\,\rm [Mpc^{-1}]$');
+            ax.set_ylabel(r'$\Delta^2_{21}\,\rm[mK^2]$');
+            ax.legend([r'Full Zeus21', r'Linear'])
+
+            ax.set_xlim([1e-2,1])
+            ax.set_ylim([1,200])
