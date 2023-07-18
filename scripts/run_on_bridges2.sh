a302eba4d15ac4d14c9b6ebb8faa3bdc5bd54131
Lisa McBride <lisaleemcb@gmail.com>
2023-07-18 08:30:19 -0400

    added sbatch script for running on supercomputing cluster

---
+++ scripts/run_on_bridges2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# use the bash shell
+set -x
+# echo each command to standard out before running it
+date
+# run the Unix 'date' command
+echo "Hello world, from Bridges-2!"
+# run the Unix 'echo' command
+#!/bin/bash
+
+#SBATCH --job-name=gold_rush
+#SBATCH --time=00-12:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=lisaleemcb@gmail.com
+#SBATCH --ntasks-per-node=12
+
+echo 'Hello, world, from Lisa!!'
+python /jet/home/emcbride/packages/gold_rush/src/gold_rush/run_mcmc.py
