347fa7ce7dd2174db2256f60cb1af3ae0db08d61
Lisa McBride <lisaleemcb@gmail.com>
2023-09-22 13:37:23 -0700

    added gaussian prior

--- src/gold_rush/fitting.py
+++ src/gold_rush/fitting.py
@@ -4,7 +4,15 @@ import emcee
 
 from multiprocessing import Pool
 
-def log_prior(params):
+def gaussian(x, mean, sigma, normed=False):
+    N = 1
+
+    if normed is True:
+        N = 1 / np.sqrt(2 * np.pi * sigma**2)
+
+    return N * np.exp(-(x - mean)**2 / (2 * sigma**2))
+
+def log_prior(params, truths, priors, priors_width):
     omegab = params[0]
     omegac = params[1]
     h_fid = params[2]
@@ -12,18 +20,21 @@ def log_prior(params):
     ns = params[4]
     #tau_fid = params[5]
 
-    if 1.8e-09 < As < 2.5e-09:
-        return 0.0
-    # if -5.0 < omegab < 0.5 and 0.0 < b < 10.0 and -10.0 < log_f < 1.0:
-    #     return 0.0
-    return -np.inf
+    if priors == 'gaussian':
+        priors_vals = np.zeros(params.size)
+
+        for i in range(params.size):
+            priors_vals[i] = np.log(gaussian(params[i], truths[i], truths[i] * priors_width))
+            print(f'prior val is: {priors_vals[i]}')
+
+    return priors_vals.sum()
 
 def log_likelihood(params, data, model, sigmas):
     log_likelihood = -(data - model(params))**2 / sigmas**2
     return log_likelihood.sum()
 
-def log_probability(params, data, model, sigmas):
-    lp = log_prior(params)
+def log_probability(params, truths, data, model, sigmas, priors, priors_width):
+    lp = log_prior(params, truths, priors, priors_width)
     if not np.isfinite(lp):
         return -np.inf
 
@@ -35,8 +46,9 @@ def log_probability(params, data, model, sigmas):
 
     return lp + lklhd
 
-def start_mcmc(truths, data, model, sigmas, backend=None,
-                    nwalkers=36, nsteps=1e5, burn_in=50):
+def start_mcmc(truths, data, model, sigmas,
+                priors='gaussian', priors_width=.1,
+                backend=None, nwalkers=36, nsteps=1e5, burn_in=50):
 
     ndim = truths.size
     nsteps = int(nsteps)
@@ -45,6 +57,7 @@ def start_mcmc(truths, data, model, sigmas, backend=None,
     print(f'ndim type is {type(nsteps)}')
 
     print(f'running emcee for {nsteps} steps with {nwalkers} walkers and {burn_in} burn in...')
+    print(f'assuming {priors} priors...')
     pos = truths * np.ones((nwalkers, ndim)) + 1e-10 * np.random.normal(scale=truths * np.ones((nwalkers, ndim)), size=(nwalkers, ndim))
 
     if backend:
@@ -53,14 +66,14 @@ def start_mcmc(truths, data, model, sigmas, backend=None,
 
         sampler = emcee.EnsembleSampler(
             nwalkers, ndim, log_probability,
-            backend=backend, args=(data, model, sigmas)
+            backend=backend, args=(truths, data, model, sigmas, priors, priors_width)
         )
 
     else:
         print('running without backend')
         sampler = emcee.EnsembleSampler(
             nwalkers, ndim, log_probability,
-            args=(data, model, sigmas)
+            args=(truths, data, model, sigmas, priors, priors_width)
         )
 
     t0 = time.time()
@@ -71,7 +84,7 @@ def start_mcmc(truths, data, model, sigmas, backend=None,
 
     sampler.reset()
     print('beginning run...')
-    
+
     t0 = time.time()
     sampler.run_mcmc(state, nsteps, progress=True)
     tf = time.time()
